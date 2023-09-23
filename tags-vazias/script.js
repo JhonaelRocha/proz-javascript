61c601be3b56dc1f82134a4bcf6c28ff948e83de
Jhonael <jhonaelrocha@gmail.com>
2023-09-22 21:16:16 -0300

    Tags Vazias adicionado

--- tags-vazias/script.js
+++ tags-vazias/script.js
@@ -3,6 +3,17 @@ const ul = document.querySelector('ul');
 const a = document.querySelector('a');
 const ol = document.querySelector('#lista-ordenada');
 titulo.innerText = 'Proz Educação'
-ul.innerText = 'Lista Vazia'
-a.innerText = 'Link'
-ol.innerText = 'Lista Ordenada Vazia'
+ul.innerHTML = 
+`
+<li>Item 1</li>
+<li>Item 2</li>
+<li>Item 3</li>
+`
+
+a.innerText = 'Link da Proz'
+ol.innerHTML =
+`
+<li><a href="https://www.youtube.com/?gl=BR">YouTube</a></li>
+<li><a href="https://www.google.com.br/">Google</a></li>
+<li><a href="https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal">Wikipedia</a></li>
+`
