847f5b8e53ce3a1a65b50723641fcb39b765c1d8
Jhonael <jhonaelrocha@gmail.com>
2023-09-22 21:23:19 -0300

    calculadora adicionada

---
+++ calculadora/script.js
@@ -0,0 +1,16 @@
+function somar(x,y){
+    console.log(`${x} mais ${y} é igual à ${x + y}`)
+}
+function subtrair(x,y){
+    console.log(`${x} menos ${y} é igual à ${x - y}`)
+}
+function multiplicar(x,y){
+    console.log(`${x} vez ${y} é igual à ${x * y}`)
+}
+function dividir(x,y){
+    console.log(`${x} dividido por ${y} é igual à ${x / y}`)
+}
+somar(4,5);
+subtrair(4,5);
+multiplicar(4,5);
+dividir(4,5);
