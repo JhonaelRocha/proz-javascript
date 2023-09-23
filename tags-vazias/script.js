87fc7c05e4798b35d80bdb3d30079b67284c9eb5
Jhonael <jhonaelrocha@gmail.com>
2023-09-22 21:10:04 -0300

    tags-vazias adicionado

---
+++ tags-vazias/script.js
@@ -0,0 +1,8 @@
+const titulo = document.getElementById('titulo');
+const ul = document.querySelector('ul');
+const a = document.querySelector('a');
+const ol = document.querySelector('#lista-ordenada');
+titulo.innerText = 'Proz Educação'
+ul.innerText = 'Lista Vazia'
+a.innerText = 'Link'
+ol.innerText = 'Lista Ordenada Vazia'
